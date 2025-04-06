a534ce07e747829d2b25bead4c1f8117364273fe
MrSloth-dev <joao8barbosa@gmail.com>
2025-04-06 02:13:04 +0100

    Expense Tracker Ongoing, add/remove/show done, need to improve UX

--- 0.ExpenseTracker/expense_tracker.py
+++ 0.ExpenseTracker/expense_tracker.py
@@ -1,8 +1,6 @@
-from ast import arg
-from re import A
 import sqlite3
 import argparse
-from datetime import datetime
+# from datetime import datetime
 
 
 def init_db() -> sqlite3.Connection:
@@ -22,40 +20,75 @@ def parse_args() -> argparse.Namespace:
   parser = argparse.ArgumentParser()
   subparsers = parser.add_subparsers(dest='command')
   add_parser = subparsers.add_parser('add')
-  _ = add_parser.add_argument('--amout', type=float, required=True)
+  _ = add_parser.add_argument('--amount', type=float, required=True)
   _ = add_parser.add_argument('--category', type=str, required=True)
   _ = add_parser.add_argument('--description', type=str, default='')
-  _ = subparsers.add_parser('stats')
+
+  show_parser = subparsers.add_parser('show')
+  group = show_parser.add_mutually_exclusive_group()
+  group = show_parser.add_argument('--total', action='store_true', help='Show totals grouped by category')
+  group = show_parser.add_argument('--entries', action='store_true', help='Show each entry and with id for removing if needed')
+
+  remove_parser = subparsers.add_parser('remove')
   return parser.parse_args()
+
+
+
+def show_entries(conn : sqlite3.Connection):
+  cursor = conn.cursor()
+  print(f"{'id':<10} {'Category':<10} {'Amount':<10}")
+  print("-" * 16)
+  _ = cursor.execute("""
+  SELECT id, category, amount  FROM expenses
+  """)
+  for row in cursor.fetchall():
+    print(f"{row[0]:<10} {row[1]:<10} {row[2]:<10.2f}")
+
+def show_totals(conn : sqlite3.Connection):
+  cursor = conn.cursor()
+  print(f"{'id':<10} {'Category':<10} {'Amount':<10}")
+  print("-" * 16)
+  _ = cursor.execute("""
+  SELECT id, category, SUM(amount) as total
+  FROM expenses
+  GROUP BY category
+  """)
+  for row in cursor.fetchall():
+    print(f"{row[0]:<10} {row[1]:<10} {row[2]:<10.2f}")
+
+def remove_expense(conn: sqlite3.Connection):
+  show_entries(conn)
+  remove = int(input("Which expense to remove?: "))
+  cursor = conn.cursor()
+  _ = cursor.execute("SELECT COUNT(*) FROM expenses")
+  size : int  = cursor.fetchone()[0]
+  if remove > size or remove < 0:
+    print("Error index")
+    return 
+  _ = cursor.execute(" DELETE FROM expenses WHERE id = ?", (remove,))
+  conn.commit()
 
 def add_expense(conn: sqlite3.Connection, amount: int, category: str, description: str):
       cursor = conn.cursor()
       _ = cursor.execute("""
-      INSERT INTO expenses (amount, category, description)
-      VALUE(?, ?, ?) """
-  , (amount, category, description))
+        INSERT INTO expenses (amount, category, description)
+        VALUES(?, ?, ?)
+      """ , (amount, category, description))
       conn.commit()
-
-
-def show_stats(conn):
-  cursor = conn.cursor()
-  _ = cursor.execute("""
-  SELECT category, SUM(amount) as total
-  FROM expenses
-  GROUP BY category
-  """)
-  for row in cursor.fecthall():
-    print(f"{row[0]}: {row[1]:.2f}")
-
-
 
 if __name__ == '__main__':
   args = parse_args()
   conn = init_db()
-  if (args.command == 'add'):
-    add_expense(conn, args.amount, args.category, args.description)
-  elif args.command == 'stats':
-    show_stats(conn)
+  match args.command:
+    case 'add':
+      add_expense(conn, args.amount, args.category, args.description)
+    case 'remove':
+      remove_expense(conn)
+    case 'show':
+      if args.total:
+        show_totals(conn)
+      if args.entries:
+        show_entries(conn)
+    case 'stats':
+      show_totals(conn)
   conn.close()
-
-
